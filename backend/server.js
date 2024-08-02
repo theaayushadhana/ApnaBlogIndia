3648eff16d8f2365864dc7cbd7459426859a4882
Aayush Adhana <developeraayush145@gmail.com>
2024-08-02 17:45:32 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -55,5 +55,5 @@ mongoose.connect(process.env.MONGO_URI)
     console.log('Error connecting to MongoDB:', error);
   });
 
-const PORT = process.env.PORT || 5000 || 6000;
+const PORT = process.env.PORT || 4173 ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
