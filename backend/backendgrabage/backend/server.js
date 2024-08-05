784a184423927bc7b8a358afe59116f837d2f650
Aayush Adhana <developeraayush145@gmail.com>
2024-08-05 20:28:54 +0530

    Change in db as well as port

--- backend/backendgrabage/backend/server.js
+++ backend/backendgrabage/backend/server.js
@@ -56,5 +56,5 @@ mongoose.connect(process.env.MONGO_URI)
     console.log('Error connecting to MongoDB:', error);
   });
 
-const PORT =  process.env.PORT;
+const PORT =  process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
